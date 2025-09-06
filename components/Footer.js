640a4577f003a7bc4d8d5223059155a487cfe196
khaled <khaledb.yahya@gmail.com>
2025-09-06 17:33:20 +0100

    Styles

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-green-900 text-cream-100 py-6 arabesque-bg">
+    <footer className="bg-green-900 text-cream-100 py-6">
       <div className="container mx-auto text-center">
         <p className="mb-2 text-lg">&copy; 2025 ثمرات الأوراق</p>
         <p className="text-sm">
