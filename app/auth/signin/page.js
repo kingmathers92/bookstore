766e158c1701b083ae154564afc8a4abc51e34be
khaled <khaledb.yahya@gmail.com>
2025-10-02 17:29:56 +0100

    Removed unused import

--- app/auth/signin/page.js
+++ app/auth/signin/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { useState } from "react";
 import { useStore } from "@/lib/store";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
