a71385a1ca9b1b42091bfcc2795bf1c5a5882188
Khaled <khaledb.yahya@gmail.com>
2025-10-16 16:45:10 +0100

    Middleware

---
+++ middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import { getServerUser } from "./lib/supabase";
+
+export async function middleware(request) {
+  const user = await getServerUser();
+
+  if (request.nextUrl.pathname.startsWith("/admin")) {
+    if (!user || user?.user_metadata?.role !== "admin") {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin/:path*"], // applies to /admin and all sub-routes
+};
